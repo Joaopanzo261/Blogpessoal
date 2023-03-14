9a03b8beda7e82bdbe002f7fd8edd454b48eda5b
João Kiala Vioka Panzo <joaopanzo261@gmail.com>
2023-03-14 12:47:31 -0300

    Blog Pessoal Parte 1

--- helloworld/helloworld/src/main/java/org/generation/helloworld/controller/HelloController.java
+++ helloworld/helloworld/src/main/java/org/generation/helloworld/controller/HelloController.java
@@ -1,6 +1,6 @@
 package org.generation.helloworld.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
